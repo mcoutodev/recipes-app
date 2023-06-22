7a8692c021789322cbdd954e2beb6420a4ba7a82
joseg-alvesg <cinhoalves85@gmail.com>
2023-06-22 18:48:31 -0300

    chore: recipes mock

---
+++ app/components/recipes/recipes.ts
@@ -0,0 +1,46 @@
+export const recipes = [
+    {
+        id: 1,
+        name: "Recipe 1",
+        instruction: "Instruction 1",
+        category: "Category 1",
+        area: "Area 1",
+        image: "https://themealdb.com/images/logo-small.png",
+        ingredients: [
+            {
+                id: 1,
+                name: "Ingredient 1",
+                quantity: 1,
+                unit: "Unit 1",
+            },
+            {
+                id: 2,
+                name: "Ingredient 2",
+                quantity: 2,
+                unit: "Unit 2",
+            },
+        ],
+    },
+    {
+        id: 2,
+        name: "Recipe 2",
+        instruction: "Instruction 2",
+        category: "Category 2",
+        area: "Area 2",
+        image: "https://themealdb.com/images/logo-small.png",
+        ingredients: [
+            {
+                id: 3,
+                name: "Ingredient 3",
+                quantity: 3,
+                unit: "Unit 3",
+            },
+            {
+                id: 4,
+                name: "Ingredient 4",
+                quantity: 4,
+                unit: "Unit 4",
+            },
+        ],
+    },
+];
